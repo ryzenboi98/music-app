b9dbac3d2a0d21f3476a1f5d5c14e58669e70d3a
ryzenboi98 <joaopro23@gmail.com>
2021-09-28 18:53:59 +0100

    Fixed structure, music app models and registered models in admin site

---
+++ music_app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from music_app.models import *
+
+# Register your models here.
+admin.site.register(Artist)
+admin.site.register(MusicGenre)
+admin.site.register(Album)
+admin.site.register(Playlist)
+admin.site.register(Song)
+admin.site.register(UserFavoriteSong)
+admin.site.register(UserFavoritePlaylist)
---
+++ music_app/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Artist(TimeStampedModel):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class MusicGenre(TimeStampedModel):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Song(TimeStampedModel):
+    name = models.CharField(max_length=50)
+    artists = models.ManyToManyField(Artist)
+    genre = models.ForeignKey(MusicGenre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Playlist(TimeStampedModel):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    songs = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.name
+
+class Album(TimeStampedModel):
+    name = models.CharField(max_length=50)
+    music_genre = models.ForeignKey(MusicGenre, default=None, null=True, blank=True, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    songs = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.name
+
+class UserFavoriteSong(TimeStampedModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favourite_songs = models.ManyToManyField(Song)
+
+class UserFavoritePlaylist(TimeStampedModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favourite_songs = models.ManyToManyField(Song)
+
+
+
+
