99e36692438ac0ebed50f64500bcf56a6eb8c95e
ryzenboi98 <joaopro23@gmail.com>
2021-09-30 17:26:02 +0100

    Improved method for checking if all songs on an albums have the same genre.

--- music_app/serializers/albums.py
+++ music_app/serializers/albums.py
@@ -3,8 +3,6 @@ from music_app.models import Album
 from .songs import SongSerializer
 
 class AlbumSerializer(serializers.HyperlinkedModelSerializer):
-    music_genre = serializers.CharField()
-    songs = SongSerializer(many=True)
     class Meta:
         model = Album
         fields = ['id', 'name', 'music_genre', 'songs']
@@ -27,6 +25,7 @@ def check_music_genres(validated_data):
     music_genre = validated_data['music_genre']
 
     if music_genre:
-        for song in songs:
-            if song.genre != music_genre:
-                raise serializers.ValidationError("Album genre doesn't match for every song.")
+        all_genres_equal = all(song.genre == music_genre for song in songs)
+
+        if not all_genres_equal:
+            raise serializers.ValidationError("Album genre doesn't match for every song.")
